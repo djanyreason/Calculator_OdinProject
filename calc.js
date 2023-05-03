af3877012f8b08d174ea5a79ebf4a8e24a936059
John Anderson <djanyreason@gmail.com>
2023-05-03 13:46:29 -0400

    Adds operate function to evaluate an operation
    This commit adds the function operate(operand1, operand2, operator)
    which takes two numbers (operand1 and operand2 variables) and a
    string of a basic mathematical operator (+, -, *, and /; operator
    variable), and returns the evaluated expression:
           operand1 operator operand2
    
    This is step 3 in the Odin Project Calculator assignment.

--- calc.js
+++ calc.js
@@ -1,6 +1,6 @@
 let firstOperand = 0;
 let secondOperand = 0;
-let operator = "";
+let currentOperator = "";
 
 function add(operand1, operand2) {
   return operand1 + operand2;
@@ -16,4 +16,17 @@ function multiply(operand1, operand2) {
 
 function divide(operand1, operand2) {
   return operand1 / operand2;
+}
+
+function operate(operand1, operand2, operator) {
+  switch(operator) {
+    case "+":
+      return add(operand1, operand2);
+    case "-":
+      return subtract(operand1, operand2);
+    case "*":
+      return multiply(operand1, operand2);
+    case "/":
+      return divide(operand1, operand2);
+  }
 }
