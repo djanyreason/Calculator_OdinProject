e607b4b5eeeeb418d2c25ce8adcbc80ebacc57f5
John Anderson <djanyreason@gmail.com>
2023-05-05 14:25:15 -0400

    Adds functionality to the operator buttons
    This commit adds a function operatorClick(operator) which takes a
    string parameter which is a calculator operator (+, -, *, /, =) and
    evaluates and updates the calculator display. This function is then
    set as the onclick action for each button of class operator, with the
    innerText as the parameter used.
    
    In order to do this, the js file also makes some updates to the
    global variables. Instead of two operands, it has just one variable,
    "total", which represents the total amount stored in the "calculator
    memory". It also uses the global variable currentOperator to store
    the most recent non-equals operator clicked in "calculator memory".
    Finally, there is a global variable inputRestart which is boolean and
    is true when the next number button press should reset the current
    value in the calculator display.
    
    This commit also slightly re-organizes the code in the JS file into
    different sections for different purposes (e.g., for display updates,
    for enabling number buttons, for enabling operator buttons).
    
    NB: I am surprised that I have not found any bugs in testing this
    functionality, and I kind of think there must be something wrong that
    I haven't caught yet.

--- calc.js
+++ calc.js
@@ -1,9 +1,51 @@
-let firstOperand = 0;
-let secondOperand = 0;
-let currentOperator = "";
+let total = 0; // Total in calculator memory
+let currentOperator = ""; //Operator to evaluate
+
+let inputRestart = true;
+// Boolean which is true if the next input should reset/overwrite the display
 
 // The "display" class div will be important for multiple functions
 const displayDiv = document.getElementsByClassName("display")[0];
+
+// Functions for updating the display
+
+// Takes a number parameter; updates the text of the "display" class div to a
+// string of the parameter, with a maximum of 1 decimal place and 8 characters
+function updateDisplay(newValue) {
+  if(typeof(newValue) != "number") return; //ensure parameter is a number
+  if(Math.abs(newValue) >= 99999999.5) { // ensure parameter is max 8 digits
+    displayDiv.innerText = "ERROR";
+    return;
+  }
+
+  if(Number.isInteger(newValue)) { // If integer, no transformation necessary
+    displayDiv.innerText = newValue.toString();
+  } else if (Math.abs(newValue) >= 999999.5) { // If at 7 digits, no decimals
+    displayDiv.innerText = Math.round(newValue).toString();
+  } else { // Short non-integer, need to ensure 1 decimal place
+    const decimalPlace = Math.abs(Math.round(newValue * 10) % 10);
+    if(decimalPlace === 0) { // If decimal is 0, do not display
+      displayDiv.innerText = Math.round(newValue).toString();
+    } else { // Add the decimal
+      if(newValue > 0) { //round down for positives
+        displayDiv.innerText = Math.floor(newValue).toString()
+                               + '.' + decimalPlace;
+      } else { //round up for negatives
+        displayDiv.innerText = Math.ceil(newValue).toString()
+                               + '.' + decimalPlace;
+      }
+    }
+  }
+}
+
+// Special updateDisplay for when a decimal point is clicked
+// Currently no functionality
+function updateDisplayDecimal() {
+  return;
+}
+
+
+// Functions for making the operator buttons work
 
 // Returns sum of 2 parameters
 function add(operand1, operand2) {
@@ -41,46 +83,68 @@ function operate(operand1, operand2, operator) {
   }
 }
 
-// Takes a number parameter; updates the text of the "display" class div to a
-// string of the parameter, with a maximum of 1 decimal place and 8 characters
-function updateDisplay(newValue) {
-  if(typeof(newValue) != "number") return; //ensure parameter is a number
-  if(Math.abs(newValue) >= 99999999.5) { // ensure parameter is max 8 digits
-    displayDiv.innerText = "ERROR";
+// Function that handles onclick for an operator button.
+function operatorClick(operator) {
+  // If inputRestart is true, that means no new number has been entered yet.
+  // In this case, if the operator button is not "=" then it should just update
+  // the operator stored in memory.
+  if((inputRestart === true) && (operator != "=")) {
+    currentOperator = operator;
     return;
   }
 
-  if(Number.isInteger(newValue)) { // If integer, no transformation necessary
-    displayDiv.innerText = newValue.toString();
-  } else if (Math.abs(newValue) >= 999999.5) { // If at 7 digits, no decimals
-    displayDiv.innerText = Math.round(newValue).toString();
-  } else { // Short non-integer, need to ensure 1 decimal place
-    const decimalPlace = Math.abs(Math.round(newValue * 10) % 10);
-    if(decimalPlace === 0) { // If decimal is 0, do not display
-      displayDiv.innerText = Math.round(newValue).toString();
-    } else { // Add the decimal
-      if(newValue > 0) { //round down for positives
-        displayDiv.innerText = Math.floor(newValue).toString()
-                               + '.' + decimalPlace;
-      } else { //round up for negatives
-        displayDiv.innerText = Math.ceil(newValue).toString()
-                               + '.' + decimalPlace;
-      }
-    }
+  // After operator button, always start fresh on next number press
+  inputRestart = true; 
+  
+  // If no saved operation and the click was not "=", store current value and 
+  // the clicked operator, and set for a new number entry
+  if((currentOperator === "") && (operator != "=")) {
+    total = parseFloat(displayDiv.innerText);
+    currentOperator = operator;
+    return;
+  }
+
+  // Evaluate the stored value and operator with the current display
+  const result = operate(total, parseFloat(displayDiv.innerText), currentOperator);
+  
+  // Update display, then copy the display value into memory: the updateDisplay
+  // function truncates to max 1 decimal, so it goes first
+  updateDisplay(result);
+  if(displayDiv.innerText === "ERROR") { // If an error, clear memory
+    total = 0;
+    currentOperator = "";
+    return;
+  } else {
+    total = parseFloat(displayDiv.innerText); 
+  }
+
+  // If the operator is "=" clear memory for operator, otherwise store operator
+  if(operator === "=") {
+    currentOperator = "";
+  } else {
+    currentOperator = operator;
   }
 }
 
-// Special updateDisplay for when a decimal point is clicked
-// Currently no functionality
-function updateDisplayDecimal() {
-  return;
+//Add onclick to number buttons
+const operatorButtons = document.getElementsByClassName("operator");
+for(let n = 0; n < operatorButtons.length; n++) {
+  operatorButtons[n].onclick = () => operatorClick(operatorButtons[n].innerText);
 }
+
+
+// Functions for making the number buttons work
 
 // Function that handles an onClick event for a number button. Takes one string
 // parameter, the number that is clicked. It adds that number to the end of the
 // display on the calculator. 
 // Does not update if the display cannot be further updated.
 function numberClick(num) {
+  if(inputRestart) { // If inputRestart is true, clear display
+    updateDisplay(0);
+    inputRestart = false;
+  }
+
   if(num === '.') {
     updateDisplayDecimal();
     return
